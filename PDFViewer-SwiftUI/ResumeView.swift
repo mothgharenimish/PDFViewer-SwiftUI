035f5942478a8a4bc706b6d89375f25211bc526d
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-05 15:13:19 +0530

    Xcode Update Changes

---
+++ PDFViewer-SwiftUI/ResumeView.swift
@@ -0,0 +1,18 @@
+//
+//  ResumeView.swift
+//  PDFViewer-SwiftUI
+//
+//  Created by Nimish Sharad Mothghare on 05/03/25.
+//
+
+import SwiftUI
+
+struct ResumeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResumeView()
+}
