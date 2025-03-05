66ca7400c26f9b673b92ea09240498e0ba39a157
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-05 14:50:51 +0530

    Initial Commit

---
+++ PDFViewer-SwiftUI/PDFViewer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFViewer_SwiftUIApp.swift
+//  PDFViewer-SwiftUI
+//
+//  Created by Nimish Sharad Mothghare on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PDFViewer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PDFViewer-SwiftUITests/PDFViewer_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  PDFViewer_SwiftUITests.swift
+//  PDFViewer-SwiftUITests
+//
+//  Created by Nimish Sharad Mothghare on 05/03/25.
+//
+
+import Testing
+@testable import PDFViewer_SwiftUI
+
+struct PDFViewer_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
